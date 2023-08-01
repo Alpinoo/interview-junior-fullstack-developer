686261d1f28e84b00760c4e830d937c3793c03e5
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-08-01 14:03:58 +0300

    form and http module to handle input and request

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitySearchComponent } from './city-search/city-search.component';
 
@@ -12,7 +13,8 @@ import { CitySearchComponent } from './city-search/city-search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

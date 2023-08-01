82fce3430209a98060b0561a1daa0ef987413d8c
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-08-01 14:02:31 +0300

    modified city search component
    -html: search input and button and created list for every incoming city
    -ts: added searchCities to send request to backend

--- interview-frontend/src/app/city-search/city-search.component.ts
+++ interview-frontend/src/app/city-search/city-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-city-search',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./city-search.component.scss']
 })
 export class CitySearchComponent {
+  searchTerm: string = '';
+  cities: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  searchCities() {
+    this.http.get<any[]>(`http://localhost:3000/cities?searchTerm=${this.searchTerm}`)
+      .subscribe((cities) => {
+        this.cities = cities;
+      });
+  }
 }
